f915344353a7ee0d20d1c5a56d7478c96020a03e
정지원 <4dakto@gmail.com>
2017-11-26 23:23:04 +0900

    test fully connected Component

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -8,4 +8,8 @@ describe('App', () => {
   it('render properly', () => {
     expect(app).toMatchSnapshot()
   })
+  
+  it('contains a Wallet component', () => {
+    expect(app.find('Connect(Wallet)').exists()).toBe(true)
+  })
 })

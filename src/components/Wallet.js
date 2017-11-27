510b8c154151b2baffe4128df135dc7bffd2a3ea
정지원 <4dakto@gmail.com>
2017-11-27 17:33:53 +0900

    Jiwon ::  mockfetch, mockstore 추

--- src/components/Wallet.js
+++ src/components/Wallet.js
@@ -33,6 +33,6 @@ export class Wallet extends Component {
 }
 
 export default connect(
-  state => ({ balance: state }),
+  state => ({ balance: state.balance }),
   { deposit, withdraw }
 )(Wallet)

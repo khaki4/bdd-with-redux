10a2b228b8712178c627d5a90b6bc4138a4b22d6
정지원 <4dakto@gmail.com>
2017-11-27 17:05:26 +0900

    add thunk test for async flow

---
+++ src/actions/bitcoin.js
@@ -0,0 +1,6 @@
+import { FETCH_BITCOIN } from './constants'
+
+export const fetchBitcoin = () => (dispatch) =>
+  fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
+    .then(response => response.json())
+    .then(json => dispatch({ type: FETCH_BITCOIN, bitcoin: json }))
